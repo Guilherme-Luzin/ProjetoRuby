7242192bf22d6fa63ed19e67292a0dbc9f9d83cf
GUilherme-Luzin <guilherme.carvalho@inventsoftware.com.br>
2021-12-06 11:09:05 -0300

    tela detalhes com dados falsos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def show
-    @cliente = [{:id=>1, :nome=>"Peter parker", :idade=>20, :email=>"peter@hotmail.com"}, 
+    @dados = [{:id=>1, :nome=>"Peter parker", :idade=>20, :email=>"peter@hotmail.com"}, 
       {:id=>2, :nome=>"Mike Wazowsk", :idade=>10, :email=>"mikezinho@gmail.com"}, 
     {:id=>3, :nome=>"Sullivan", :idade=>12, :email=>"sumonstrao@outlook.com"}, 
     {:id=>4, :nome=>"Milles Moralles", :idade=>18, :email=>"milles@yahoo.com.br"}]
